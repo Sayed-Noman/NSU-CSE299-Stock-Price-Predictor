5276b09e9d07d28b73999858293a5315559bb5b1
Sayed Noman <Sayed.siddik@northsouth.edu>
2021-05-08 20:26:24 +0600

    #2 landing,about,services,contact page added

--- guessstock/home/urls.py
+++ guessstock/home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from  home import  views
 
 urlpatterns = [
-    
+    path('',views.landing,name='landing-page'),
+    path('about/',views.about,name='about-page'),
+    path('contact/',views.contact,name='contact-page'),
+    path('services/',views.services,name='services-page'),
+
 ]
--- guessstock/home/views.py
+++ guessstock/home/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def landing(request):
+    '''
+    This will redirect the url to the landing page
+    :type request: HttpResponse
+    :param request: Takes the request to show home.html
+    
+    '''
+    return render(request, 'home/landing.html')
+
+def about(request):
+    '''
+    This will redirect the url to the about page
+    :type request: HttpResponse
+    :param request: Takes the request to show home.html
+    
+    '''
+    return render(request, 'home/about.html')
+
+def contact(request):
+    '''
+    This will redirect the url to the contact page
+    :type request: HttpResponse
+    :param request: Takes the request to show home.html
+    
+    '''
+    return render(request, 'home/contact.html')
+
+def services(request):
+    '''
+    This will redirect the url to the services page
+    :type request: HttpResponse
+    :param request: Takes the request to show home.html
+    
+    '''
+    return render(request, 'home/services.html')
