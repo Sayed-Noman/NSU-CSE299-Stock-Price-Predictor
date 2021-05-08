05711e1d77d14620cf2fb9806f0eeff6dd9ab1cf
Sayed Noman <Sayed.siddik@northsouth.edu>
2021-05-08 22:27:08 +0600

    predictor apss major files added, graph and table left to work on

--- guessstock/predictor/urls.py
+++ guessstock/predictor/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from  predictor import  views
 
 urlpatterns = [
-    
+    path('guess-stock/',views.guess_stock,name='guees_stock'),
+    path('amazon-predict/',views.amazon_prediction,name='amazon_predict'),
+    path('facebook-predict/',views.facebook_prediction,name='facebook_predict'),
+    path('tesla-predict/',views.tesla_prediction,name='tesla_predict'),
+    path('google-predict/',views.google_prediction,name='google_predict'),
+    path('microsoft-predict/',views.microsoft_prediction,name='microsoft_predict'),
+    path('apple-predict/',views.apple_prediction,name='apple_predict'),
+    path('csv-to-graph/',views.csv_to_graph,name='csv_to_graph'),
 ]
--- guessstock/predictor/views.py
+++ guessstock/predictor/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def guess_stock(request):
+    '''
+    This will redirect the url to the stock home page page
+    :type request: HttpResponse
+    :param request: Takes the request to show guess_stock.html
+    
+    '''
+    return render(request, 'predictor/guess_stock.html')
+
+def amazon_prediction(request):
+    '''
+    This will redirect the url to the amazon_prediction page
+    :type request: HttpResponse
+    :param request: Takes the request to show amazon_predictionhtml
+    
+    '''
+    return render(request, 'predictor/amazon_prediction.html')
+
+
+def google_prediction(request):
+    '''
+    This will redirect the url to the google_prediction page
+    :type request: HttpResponse
+    :param request: Takes the request to show google_prediction.html
+    
+    '''
+    return render(request, 'predictor/google_prediction.html')
+
+
+def microsoft_prediction(request):
+    '''
+    This will redirect the url to the microsoft_prediction page
+    :type request: HttpResponse
+    :param request: Takes the request to show microsoft_prediction.html
+    
+    '''
+    return render(request, 'predictor/microsoft_prediction.html')
+
+
+def tesla_prediction(request):
+    '''
+    This will redirect the url to the tesla_prediction page
+    :type request: HttpResponse
+    :param request: Takes the request to show tesla_prediction.html
+    
+    '''
+    return render(request, 'predictor/tesla_prediction.html')
+
+def facebook_prediction(request):
+    '''
+    This will redirect the url to the facebook_prediction page
+    :type request: HttpResponse
+    :param request: Takes the request to show facebook_prediction.html
+    
+    '''
+    return render(request, 'predictor/facebook_prediction.html')
+
+def apple_prediction(request):
+    '''
+    This will redirect the url to the tesla_prediction page
+    :type request: HttpResponse
+    :param request: Takes the request to show apple_prediction.html
+    
+    '''
+    return render(request, 'predictor/apple_prediction.html')
+
+
+def csv_to_graph(request):
+    '''
+    This will redirect the url to the stock home page page
+    :type request: HttpResponse
+    :param request: Takes the request to show guess_stock.html
+    
+    '''
+    return render(request, 'predictor/csv_to_graph.html')
